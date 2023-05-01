19d3b94b65fb37fcb5d4c0637d0f44ddfd4a58e6
“whoiskatrin” <katjareznikova@gmail.com>
2023-05-01 20:17:19 +0100

    added basic interface for qdranr

--- vectordbs/types.py
+++ vectordbs/types.py
@@ -1,35 +1,43 @@
-"""CREDIT: Originated from GPT index code"""
-"""Vector store index types."""
-
-
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Protocol, runtime_checkable
-
 from enum import Enum
+
+@dataclass
+class DocumentChunk:
+    document_id: str
+    text: str
+    vector: List[float]
+
+@dataclass
+class DocumentMetadataFilter:
+    field_name: str
+    gte: Optional[int] = None
+    lte: Optional[int] = None
+
+@dataclass
+class DocumentChunkWithScore(DocumentChunk):
+    score: float
+@dataclass
+class QueryResult:
+    data: Optional[List[Any]] = None
+    similarities: Optional[List[float]] = None
+    ids: Optional[List[str]] = None
+@dataclass
+class QueryWithEmbedding:
+    text: str
+    vector: List[float]
 
 @dataclass
 class VectorStoreData:
     id: str
     data: dict
     embedding: List[float]
-
-
-@dataclass
-class VectorStoreQueryResult:
-    """Vector store query result."""
-
-    data: Optional[List[Any]] = None
-    similarities: Optional[List[float]] = None
-    ids: Optional[List[str]] = None
-
-
 class VectorStoreQueryMode(str, Enum):
     """Vector store query mode."""
 
     DEFAULT = "default"
     SPARSE = "sparse"
     HYBRID = "hybrid"
-
 
 @dataclass
 class VectorStoreQuery:
@@ -46,7 +54,6 @@ class VectorStoreQuery:
 
     # NOTE: only for hybrid search (0 for bm25, 1 for vector search)
     alpha: Optional[float] = None
-
 
 @runtime_checkable
 class VectorStore(Protocol):
@@ -66,6 +73,6 @@ class VectorStore(Protocol):
     def query(
         self,
         query: VectorStoreQuery,
-    ) -> VectorStoreQueryResult:
+    ) -> QueryResult:
         """Query vector store."""
         ...
